d9d2f69bfbc9130a36fcc47f3b09e365310da305
Arka <arkapatra31@gmail.com>
2021-08-30 13:58:42 +0530

    Update nums as {}

--- 12_Backtracking/AllPermutation.cpp
+++ 12_Backtracking/AllPermutation.cpp
@@ -21,9 +21,7 @@ using namespace std;
 
     int main(){
         vector<int> nums;
-        nums.push_back(1);
-        nums.push_back(2);
-        nums.push_back(3);
+        nums = {1,2,3};
         vector<vector<int>> ans = permute(nums);
         for(int i=0; i<ans.size(); i++){
             for(int j=0; j<ans[0].size(); j++)

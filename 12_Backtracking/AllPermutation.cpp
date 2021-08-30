e866da4fefcd0a62ab0d2456d17152f5ef32cf6a
Arka <arkapatra31@gmail.com>
2021-08-30 13:50:20 +0530

    Add Permutation in Backtracking

---
+++ 12_Backtracking/AllPermutation.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+    vector<vector<int>> ans;
+    void solve(vector<int> &nums, int i){
+        if(i==nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+        for(int x=i; x<nums.size(); x++){
+            swap(nums[i], nums[x]);
+            solve(nums, i+1);
+            swap(nums[i], nums[x]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums, 0);
+        return ans;
+    }
+
+    int main(){
+        vector<int> nums;
+        nums.push_back(1);
+        nums.push_back(2);
+        nums.push_back(3);
+        vector<vector<int>> ans = permute(nums);
+        for(int i=0; i<ans.size(); i++){
+            for(int j=0; j<ans[0].size(); j++)
+                cout<<ans[i][j]<<", ";
+            cout<<endl;
+        }
+    }
